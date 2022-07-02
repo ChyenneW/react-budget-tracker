f1c663c3f9bfff242dd2de70fdb689ffe6629baa
ChyenneW <williamscheyenne771@gmail.com>
2022-07-02 15:56:57 -0500

    Create ExpenseTotal.js

---
+++ src/components/ExpenseTotal.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const ExpenseTotal = () => {
+    return (
+        <div>
+            <span>Spent: $1000</span>
+        </div>
+    );
+}
+
+export default ExpenseTotal;

43903af3f6970b442015a711642b9f4e5f53db81
ChyenneW <williamscheyenne771@gmail.com>
2022-07-03 18:44:05 -0500

    Add bootstrap CSS inline-styling to ExpenseList and ExpenseItem

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -5,8 +5,8 @@ const ExpenseItem = (props) => {
     return (
         <li>
             {props.name}
-            <div>
-                <span>
+            <div className='list-group-item d-flex justify-content-between align-items-center'>
+                <span className='badge badge-primary badge-pill mr-3'>
                     ${props.cost}
                 </span>
                 <TiDelete size="1.5em"></TiDelete>

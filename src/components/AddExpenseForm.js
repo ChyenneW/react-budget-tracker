f8532c8d170cdf4f1a9da5e5065b7643c84c1dd6
ChyenneW <williamscheyenne771@gmail.com>
2022-07-04 08:15:07 -0500

    Add form to AddExpenseForm

--- src/components/AddExpenseForm.js
+++ src/components/AddExpenseForm.js
@@ -2,9 +2,27 @@ import React from "react";
 
 const AddExpenseForm = () => {
     return (
-        <div>
-            <p>Hi</p>
-        </div>
+        <form>
+            <div>
+                <div>
+                    <label for="name">Name</label>
+                    <input
+                        required="required"
+                        type="text"
+                    ></input>
+                </div>
+                <div>
+                    <label for="cost">Cost</label>
+                    <input required="required" type="text"></input>
+                </div>
+            </div>
+
+            <div>
+                <div>
+                    <button type="submit">Save</button>
+                </div>
+            </div>
+        </form>
     );
 }
 

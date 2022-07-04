4d5cd6dd1f8dec2c22bbe314bf4b57c1a9c30c87
ChyenneW <williamscheyenne771@gmail.com>
2022-07-04 08:26:16 -0500

    Add bootstrap inline CSS styling

--- src/components/AddExpenseForm.js
+++ src/components/AddExpenseForm.js
@@ -3,23 +3,25 @@ import React from "react";
 const AddExpenseForm = () => {
     return (
         <form>
-            <div>
-                <div>
+            <div className="row">
+                <div className="col-sm">
                     <label for="name">Name</label>
                     <input
                         required="required"
                         type="text"
+                        className="form-control"
+                        id="name"
                     ></input>
                 </div>
-                <div>
+                <div className="col-sm">
                     <label for="cost">Cost</label>
-                    <input required="required" type="text"></input>
+                    <input required="required" type="text" className="form-control" id="cost"></input>
                 </div>
             </div>
 
-            <div>
-                <div>
-                    <button type="submit">Save</button>
+            <div className="row">
+                <div className="col-sm">
+                    <button type="submit" className="btn btn-primary mt-3">Save</button>
                 </div>
             </div>
         </form>

9c537f8bc36ac3ed9a09e2d3f880bd13cc53f550
ChyenneW <williamscheyenne771@gmail.com>
2022-07-04 09:40:51 -0500

    Revert "Render budget from AppContext"
    This reverts commit d93d509e2844e5c975a263b94bbc2fdac15ecde7.

--- src/components/Budget.js
+++ src/components/Budget.js
@@ -1,12 +1,9 @@
-import React, { useContext } from "react";
-import { AppContext } from "../context/AppContext";
+import React from "react";
 
 const Budget = () => {
-    const { budget } = useContext(AppContext);
-
     return (
         <div className="alert alert-secondary">
-            <span>Budget: ${budget}</span>
+            <span>Budget: $2000</span>
         </div>
     );
 };

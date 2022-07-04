69392294a529b9c6f921c0fe3a33972d2490aaba
ChyenneW <williamscheyenne771@gmail.com>
2022-07-04 16:56:31 -0500

    Add toggled textarea to input new buget

--- src/components/Budget.js
+++ src/components/Budget.js
@@ -1,15 +1,21 @@
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 import { AppContext } from "../context/AppContext";
 import { FiEdit } from 'react-icons/fi';
 
 
 const Budget = () => {
     const { budget } = useContext(AppContext);
+    const [allowEdit, setEditable] = useState("false");
+
+    const handleBudgetEdit = () => {
+        setEditable(!allowEdit);
+    };
 
     return (
         <div className="alert alert-secondary d-flex justify-content-between align-items-center">
             <span>Budget: ${budget}</span>
-            <FiEdit size="1.5em"></FiEdit>
+            <textarea className={allowEdit ? "d-flex" : "d-none"}></textarea>
+            <FiEdit size="1.5em" onClick={handleBudgetEdit}></FiEdit>
         </div>
     );
 };

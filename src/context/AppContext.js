ee06d66f967fe387d504590f0ce2181ecaaab55e
ChyenneW <williamscheyenne771@gmail.com>
2022-07-04 09:02:10 -0500

    Create Provider component

--- src/context/AppContext.js
+++ src/context/AppContext.js
@@ -1,6 +1,8 @@
+import { useReducer } from "react";
+
 const initialState = {
     budget: 2000,
-    expense: [
+    expenses: [
         { id: 12, name: 'shopping', cost: 40 },
         { id: 13, name: 'holiday', cost: 400 },
         { id: 14, name: 'car service', cost: 50 },
@@ -8,3 +10,13 @@ const initialState = {
 };
 
 export const AppContext = createContext();
+
+const AppProvider = (props) => {
+    const [state, dispatch] = useReducer(AppReducer, initialState);
+
+    return (
+        <AppContext.Provider value={{ budget: state.budget, expenses: state.expenses, dispatch }}>
+            {props.cildren}
+        </AppContext.Provider>
+    );
+};

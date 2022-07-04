ccafbe67a92a7c3ac2bf3d7c7f838ec1900e78dc
ChyenneW <williamscheyenne771@gmail.com>
2022-07-04 16:14:55 -0500

    Import FiEdit as point to edit budget

--- src/components/Budget.js
+++ src/components/Budget.js
@@ -1,12 +1,15 @@
 import React, { useContext } from "react";
 import { AppContext } from "../context/AppContext";
+import { FiEdit } from 'react-icons/fi';
+
 
 const Budget = () => {
     const { budget } = useContext(AppContext);
 
     return (
-        <div className="alert alert-secondary">
+        <div className="alert alert-secondary d-flex justify-content-between align-items-center">
             <span>Budget: ${budget}</span>
+            <FiEdit size="1.5em"></FiEdit>
         </div>
     );
 };

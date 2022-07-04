3903b40a3e7237eebad0a87feeb8a839436ecacd
ChyenneW <williamscheyenne771@gmail.com>
2022-07-04 09:49:37 -0500

    Fix typo that prevented loading

--- src/context/AppContext.js
+++ src/context/AppContext.js
@@ -24,7 +24,7 @@ export const AppProvider = (props) => {
 
     return (
         <AppContext.Provider value={{ budget: state.budget, expenses: state.expenses, dispatch }}>
-            {props.cildren}
+            {props.children}
         </AppContext.Provider>
     );
 };

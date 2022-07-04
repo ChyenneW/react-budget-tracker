4cfa6bb6f29621a68b785215e99eba391ae11678
ChyenneW <williamscheyenne771@gmail.com>
2022-07-04 08:28:20 -0500

    Update App inline bootstrap CSS

--- src/App.js
+++ src/App.js
@@ -31,9 +31,9 @@ function App() {
         </div>
       </div>
 
-      <h3>Add Expense</h3>
-      <div>
-        <div>
+      <h3 className='mt-3'>Add Expense</h3>
+      <div className='row mt-3'>
+        <div className='col-sm'>
           <AddExpenseForm />
         </div>
       </div>
